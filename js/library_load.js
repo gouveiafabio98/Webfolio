fe93f87e58afcff8d969eec9a9e6b0f06fb43426
Fábio Gouveia Silva <49789907+gouveiafabio98@users.noreply.github.com>
2023-09-24 17:58:06 +0100

    update

--- js/library_load.js
+++ js/library_load.js
@@ -10,24 +10,28 @@ function initLocomotiveScroll() {
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
         mobile: {
-           smooth: true,
-           breakpoint: 0
-       },
-       tablet: {
-           smooth: true,
-           breakpoint: 0
-       }
+            smooth: true,
+            breakpoint: 0
+        },
+        tablet: {
+            smooth: true,
+            breakpoint: 0
+        }
     });
 }
 
 window.addEventListener("load", (event) => {
     if (typeof Masonry !== 'undefined') {
-        msnry = new Masonry('.grid', {
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-sizer',
-            gutter: '.gutter-sizer',
-            percentPosition: true
-        });
+
+        var grid = document.getElementsByClassName('grid');
+        for (var i = 0; i < grid.length; i++) {
+            msnry = new Masonry(grid[i], {
+                itemSelector: '.grid-item',
+                columnWidth: '.grid-sizer',
+                gutter: '.grid-gutter',
+                percentPosition: true
+            });
+        }
 
         msnry.on('layoutComplete', function () {
             initLocomotiveScroll();
