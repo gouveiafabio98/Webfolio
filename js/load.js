b74afa50bc0fb99ff2dd84a4fefcfbfa4b861609
Fábio Gouveia Silva <49789907+gouveiafabio98@users.noreply.github.com>
2023-09-30 23:47:41 +0100

    up

---
+++ js/load.js
@@ -0,0 +1,22 @@
+var loading = document.getElementById("loading").children[1];
+var currentLoad = 0,
+    loadBlock = true;
+
+setTimeout(loadScreen, 50);
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        loadBlock = false;
+    }, 500);
+});
+
+function loadScreen() {
+    if (!loadBlock) {
+        loading.innerHTML = "100/100";
+        document.getElementById("loading").classList.add("done");
+    } else if (currentLoad < 99) {
+        currentLoad++;
+        loading.innerHTML = currentLoad + "/100";
+        setTimeout(loadScreen, 50);
+    }
+}
